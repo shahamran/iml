743b3191a448aaca64c6faa0525efa1ce10f59b2
Ran Shaham <shahamran91@gmail.com>
2017-06-19 00:29:59 +0300

    (ex5) starting to work. add q2 trees

--- ex5/ID3.py
+++ ex5/ID3.py
@@ -1,9 +1,14 @@
 import numpy as np
-from anytree import NodeMixin
+from anytree import NodeMixin, RenderTree
+from anytree.dotexport import RenderTreeGraph
 import pptree
 
+
 class Node(NodeMixin):
+    id = 0
     def __init__(self, value, feature=None, parent=None):
+        self._id = Node.id
+        Node.id += 1
         if feature is None:
             self.value = value
             self.name = value
@@ -17,6 +22,7 @@ class Node(NodeMixin):
             self.name = 'x_%d=?' % feature
         self.parent = parent
 
+
 def argmax(f, iterable):
     best_value = -np.inf
     best_index = 0
@@ -27,149 +33,28 @@ def argmax(f, iterable):
             best_index = x
     return best_index
 
-def create_node(value, feature=None):
-    if feature is None:
-        return {'_i': None, 'value': value}
-    else:
-        return dict(_i=feature, **value)
 
+class ID3Classifier:
 
-def entropy(label_values, S):
-    if S is None or S.shape[0] == 0:
-        return 0
-    m = S.shape[0]
-    result = 0
-    for c in label_values:
-        # compute the probability for label `c`
-        p = sum(S.label == c) / m
-        # if p=0 then entropy is 0
-        result -= p * np.log(p) if p > 0 else 0
-    return result
-
-def Gain(feature_values, label_values, S, i, H=entropy):
-    m = S.shape[0]
-    c = 0
-    for v in feature_values:
-        # Sv_indices is the indices of samples in which the feature `i`
-        # gets the value `v`
-        Sv_indices = S.loc[:, i] == v
-        Sv = S.loc[Sv_indices, :]
-        c += Sv.shape[0] / m * H(label_values, Sv)
-    return H(label_values, S) - c
-
-def Helper(S, feature_values, label_values, features, max_height):
-    sample_size = S.shape[0]
-    best_label, best_label_score = 0, 0
-    # check if all labels get the same value. if not, compute the label
-    # which has the maximal number of examples
-    for label_value in label_values:
-        sub_sample_size = sum(S.label == label_value)
-        if sub_sample_size == sample_size:
-            return Node(label_value)
-        if sub_sample_size > best_label_score:
-            best_label_score = sub_sample_size
-            best_label = label_value
-    if features is None or len(features) == 0 or max_height == 0:
-        return Node(best_label)
-
-    # take the feature that maximizes the gain and remove it from the
-    # feature set
-    j = argmax(lambda i: Gain(feature_values, label_values, S, i), features)
-    new_features = features - {j}
-    temp = dict()
-    # for every possible feature value, create a sub-tree
-    for feature_value in feature_values:
-        j_indices = S.loc[:, j] == feature_value
-        temp[feature_value] = Helper(S.loc[j_indices, :],
-                                     feature_values,
-                                     label_values,
-                                     new_features, max_height-1)
-    return Node(temp, j)
-
-def train(S, max_height=None):
-    features = set(S.columns[:-1])
-    if max_height is None:
-        max_height = len(features)
-    feature_values = np.unique(S.loc[:, 0])
-    label_values = np.unique(S.label)
-    return Helper(S, feature_values, label_values, features, max_height)
-
-def predict_one(tree, s):
-    temp = tree
-    while temp._i is not None:
-        for child in temp.children:
-            if child.value == s[temp._i]:
-                temp = child
-                break
-    return temp.name
-
-def predict(tree, S):
-    m = S.shape
-    if len(m) == 1:
-        return predict_one(tree, S)
-    else:
-        m = m[0]
-        predictions = [None] * m
-        for i in range(m):
-            predictions[i] = predict_one(tree, S.iloc[i])
-        return predictions
-
-def show(tree):
-    pptree.print_tree(tree)
-
-
-
-
-
-
-
-
-
-
-
-
-class ID3:
-    """
-    represents the ID3 classifier and implements its important methods
-    """
-
-    def __init__(self, feature_values, label_values):
-        """
-        creates a new classifier
-        :param feature_values: (array_like) possible feature values range
-        :param label_values: (array_like) possible label values range
-        """
-        self.root = None
-        self.feature_values = feature_values
+    def __init__(self, label_values, feature_values):
+        self.tree = None
         self.label_values = label_values
+        self.feature_values = feature_values
 
-    def entropy(label_values, S):
-        """
-        computes the entropy of a given set
-        :param label_values: (array_like) possible values for the labels
-        :param S: a set of examples
-        :return: sum over i of -p_i*log(p_i)
-        """
+    def entropy(self, S):
         if S is None or S.shape[0] == 0:
             return 0
         m = S.shape[0]
         result = 0
-        for c in label_values:
+        for c in self.label_values:
             # compute the probability for label `c`
             p = sum(S.label == c) / m
             # if p=0 then entropy is 0
             result -= p * np.log(p) if p > 0 else 0
         return result
 
-    def Gain(self, S, i, H=entropy):
-        """
-        computes the gain of the feature i and example set S.
-        by default, computes the information gain
-        :param S: (pandas dataframe) the examples set
-        :param i: (non-negative int) the feature's index
-        :param H: (callable) how to compute the gain
-        :return: the gain of feature i w.r.t S
-        """
+    def Gain(self, S, i):
+        H = self.entropy
         m = S.shape[0]
         c = 0
         for v in self.feature_values:
@@ -177,76 +62,96 @@ class ID3:
             # gets the value `v`
             Sv_indices = S.loc[:, i] == v
             Sv = S.loc[Sv_indices, :]
-            c += Sv.shape[0] / m * H(self.label_values, Sv)
-        return H(self.label_values, S) - c
+            c += Sv.shape[0] / m * H(Sv)
+        return H(S) - c
 
-    def ID3(self, S, A, height):
-        """
-        implementation of the ID3 (recursive) algorithm
-        :param S: (dataframe) the examples set
-        :param A: (set) the features set
-        :param height: (int) maximal height of the resulting tree
-        :return: a decision tree of height <= `height` as chosen by ID3 with
-                 information gain as a gain measurement
-        """
-        m = S.shape[0]
-        max_c, max_c_val = 0, 0
+    def _helper(self, S, features, max_height):
+        sample_size = S.shape[0]
+        best_label, best_label_score = 0, 0
         # check if all labels get the same value. if not, compute the label
         # which has the maximal number of examples
-        for c in self.label_values:
-            Sc = sum(S.label == c)
-            if Sc == m:
-                return DTree.create_tree(c)
-            if Sc > max_c_val:
-                max_c_val = Sc
-                max_c = c
-        if A is None or len(A) == 0 or height == 0:
-            return DTree.create_tree(max_c)
-        # take the feature that maximizes the gain and remove it from the
-        # feature set (denoted by new_A)
-        j = np.argmax([self.Gain(S, i) for i in A])
-        new_A = A - {j}
-        trees = [None] * len(self.feature_values)
-        # for every possible feature value, create a sub-tree
-        for i, v in enumerate(self.feature_values):
-            j_indices = S.loc[:, j] == v
-            trees[i] = (self.ID3(S.loc[j_indices, :], new_A, height - 1), v)
-        return DTree.create_tree(trees, j)
+        for label_value in self.label_values:
+            sub_sample_size = sum(S.label == label_value)
+            if sub_sample_size == sample_size:
+                return Node(label_value)
+            if sub_sample_size > best_label_score:
+                best_label_score = sub_sample_size
+                best_label = label_value
+        if features is None or len(features) == 0 or max_height == 0:
+            return Node(best_label)
 
-    def train(self, S, height=None):
-        """
-        trains the algorithm on a given set
-        :param S: data set where the last column is the labels' column
-        :param height: the maximal tree's height (optional) - default value
-                       is the number of features (full tree)
-        :return: a decision tree fitted to the given data-set using ID3
-        """
+        # take the feature that maximizes the gain and remove it from the
+        # feature set
+        j = argmax(lambda i: self.Gain(S, i), features)
+        new_features = features - {j}
+        temp = dict()
+        # for every possible feature value, create a sub-tree
+        for feature_value in self.feature_values:
+            j_indices = S.loc[:, j] == feature_value
+            temp[feature_value] = self._helper(S.loc[j_indices, :],
+                                               new_features,
+                                               max_height-1)
+        return Node(temp, j)
+
+    def fit(self, S, max_height=None):
+        Node.id = 0
         features = set(S.columns[:-1])
-        if height is None:
-            height = len(features)
-        self.root = self.ID3(S, features, height)
+        if max_height is None:
+            max_height = len(features)
+        self.tree = self._helper(S, features, max_height)
         return self
 
+    def _predict_one(self, s):
+        temp = self.tree
+        while temp._i is not None:
+            for child in temp.children:
+                if child.value == s[temp._i]:
+                    temp = child
+                    break
+        return temp.name
+
     def predict(self, S):
-        """
-        predict the labels of the given data set according to the fitted tree
-        :param S: a data set to predict on
-        :return: an array of predictions
-        """
         m = S.shape
-        # if we're dealing with one sample predict and return
         if len(m) == 1:
-            return self.root[S]
-        # otherwise, m is the number of samples
-        m = m[0]
-        predictions = [None] * m
-        # get a prediction for each sample (see DTree's implementation)
-        for i in range(m):
-            predictions[i] = self.root[S.iloc[i]]
-        return predictions
+            return self._predict_one(S)
+        else:
+            m = m[0]
+            predictions = [None] * m
+            for i in range(m):
+                predictions[i] = self._predict_one(S.iloc[i])
+            return predictions
+
+    def show(self):
+        pptree.print_tree(self.tree)
+
+    def save_to_file(self, filename):
+
+        def edgeattrfunc(node, child):
+            return 'label=' + str(child.value)
+
+        def nodeattrfunc(node):
+            attr = 'shape='
+            if node.is_leaf:
+                attr += 'circle'
+            else:
+                attr += 'rectangle'
+            return attr
+
+        def nodenamefunc(node):
+            if node.is_leaf:
+                name = node.name
+                #name = num_to_label[node.name]
+            else:
+                name = node.name
+            if node.is_root:
+                name += '\n' + 'Height(%d)' % node.height
+            name += '\n' + 'id(%d)' % node._id
+            return name
+
+        RenderTreeGraph(self.tree,
+                        nodeattrfunc=nodeattrfunc,
+                        edgeattrfunc=edgeattrfunc,
+                        nodenamefunc=nodenamefunc).to_picture(filename)
 
     def __str__(self):
-        """
-        pretty print
-        """
-        return str(self.root)
+        return str(RenderTree(self.tree))
--- ex5/ex5.py
+++ ex5/ex5.py
@@ -2,9 +2,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import ID3
-from anytree.dotexport import RenderTreeGraph
 from os import path
+import ID3
 
 # constants
 TRAIN_FILE = 'train.txt'
@@ -13,58 +12,42 @@ CSV_PARAMS = {'delim_whitespace': True, 'header': None}
 FEATURE_VALUES = ['y', 'n', 'u']
 TREES_DIR = 'trees'
 
-def TREE_FILE(name):
-    return path.join(TREES_DIR, name)
+def TREE_FILE(question, name):
+    return path.join(TREES_DIR, 'q%d_%s.svg' % (question, name))
 
 # read and tidy up the data
 train_data = pd.read_csv(TRAIN_FILE, **CSV_PARAMS)
 validation_data = pd.read_csv(VALIDATION_FILE, **CSV_PARAMS)
+
 # change the last column's name to "label"
 for data in [train_data, validation_data]:
     data.rename(columns={(data.shape[1]-1): 'label'}, inplace=True)
-# map possible label values to the set {0, ... , d-1}
+
 labels = pd.unique(train_data.label)
-num_to_label = {i: label for i, label in enumerate(labels)}
-label_to_num = {label: i for i, label in enumerate(labels)}
-train_data.label = train_data.label.map(label_to_num)
-validation_data.label = validation_data.label.map(label_to_num)
-# num_questions == number of "features"
 features = train_data.columns[:-1]
+
 d = len(features)
 d_values = np.array(range(d+1))
+
+# initialize the ID3 class
+T = ID3.ID3Classifier(labels, FEATURE_VALUES)
 
 m_train = train_data.shape[0]
 m_valid = validation_data.shape[0]
 train_error = [None] * (d+1)
 valid_error = train_error.copy()
 for max_height in d_values:
-    T = ID3.train(train_data, max_height)
-    train_error[max_height] = sum(ID3.predict(T, train_data) !=
+    train_predictions = T.fit(train_data, max_height).predict(train_data)
+    valid_predictions = T.predict(validation_data)
+    train_error[max_height] = sum(train_predictions !=
                                   train_data.label) / m_train
-    valid_error[max_height] = sum(ID3.predict(T, validation_data) !=
+    valid_error[max_height] = sum(valid_predictions !=
                                   validation_data.label) / m_valid
-    # print('Tree of height <= %d' % depth)
-    # ID3.show(T)
+    T.save_to_file(TREE_FILE(question=2, name=str(max_height)))
 
-
-    # print('\n' + 'max height = %d' % depth)
-    # print('nodes = %d' % T.root._nodes)
-    # print('height = %d' % T.root._height)
-    # print('root = %s' % T.root.name)
-# pptree.print_tree(T.root, '_children')
-def get_answer(node, child):
-    return 'label=' + str(child.value)
-def node_name(node):
-    if node.name in num_to_label:
-        name = num_to_label[node.name]
-    else:
-        name = node.name
-    return '%s (%d)' % (name, node.depth)
-# RenderTreeGraph(T, nodenamefunc=node_name, edgeattrfunc=get_answer).to_picture(
-#     'test.png')
 plt.plot(d_values, train_error, label='training')
 plt.plot(d_values, valid_error, label='validation')
-plt.xlabel('maximal height of the tree (d)')
+plt.xlabel('maximal tree height (d)')
 plt.ylabel('error')
 plt.legend()
 plt.show()
